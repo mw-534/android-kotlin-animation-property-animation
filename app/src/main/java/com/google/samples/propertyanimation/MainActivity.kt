e4d4e30b81a856ba1cfbb177ab8c7f169d022cae
ASUS <m>
2021-11-05 12:23:55 +0100

    Implemented color animation using a general property.

--- app/src/main/java/com/google/samples/propertyanimation/MainActivity.kt
+++ app/src/main/java/com/google/samples/propertyanimation/MainActivity.kt
@@ -20,6 +20,7 @@ import android.animation.Animator
 import android.animation.AnimatorListenerAdapter
 import android.animation.ObjectAnimator
 import android.animation.PropertyValuesHolder
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -129,6 +130,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun colorizer() {
+        val animator = ObjectAnimator.ofArgb(star.parent,
+            "backgroundColor", Color.BLACK, Color.RED)
+        animator.duration = 500
+        animator.repeatCount = 1
+        animator.repeatMode = ObjectAnimator.REVERSE
+        animator.disableViewDuringAnimation(colorizeButton)
+        animator.start()
     }
 
     private fun shower() {
